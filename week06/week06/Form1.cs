36834b870492ca88db34f78444b7e17259c5ef31
Bálint János Pomázi <balint.pomazi@stud.uni-corvinus.hu>
2021-10-27 17:46:54 +0200

    4.feladat

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,15 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.Entities;
 using week06.MnbServiceReference;
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        private BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
             var mnbService = new MNBArfolyamServiceSoapClient();
 
             var request = new GetExchangeRatesRequestBody()
